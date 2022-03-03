a7bc039acaf8b81974e76541088a13a7ce12dc64
manho <86037446+manho30@users.noreply.github.com>
2022-03-03 21:16:43 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 // redirect to documentation page 
 <?php 
-header("Location: https://manho30.github.io/mycovidapi/");
+echo json_encode(array(
+    "ok" => true,
+    "status" => 400,
+    "message" => "Please Specify a value! Here will bring you to the API documentation!",
+    ));
+header("Refresh: 5; Location: https://manho30.github.io/mycovidapi/");
 exit();
 ?>
