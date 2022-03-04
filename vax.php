fcfb92ff50bca1fef755c1bf545b65a0d5c12dc9
manho <86037446+manho30@users.noreply.github.com>
2022-03-04 10:40:40 +0800

    Update vax.php

--- vax.php
+++ vax.php
@@ -50,7 +50,7 @@ if (empty($date_request)) {
 
         header("Content-type: application/json");
         
-        http_response_code(200)
+        http_response_code(200);
 
         echo json_encode(array(
                 "ok"=> true, 
