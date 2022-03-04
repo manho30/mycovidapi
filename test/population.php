b156f8787033be048a42a29f4667747f57db5487
manho <86037446+manho30@users.noreply.github.com>
2022-03-04 20:51:44 +0800

    Update population.php

--- test/population.php
+++ test/population.php
@@ -1,5 +1,5 @@
 <?php
-$url = "https://github.com/MoH-Malaysia/covid19-public/blob/main/static/population.csv";
+$url ="https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/epidemic/clusters.csv";
 
 $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_URL, $url);
@@ -13,7 +13,9 @@ $resp = curl_exec($curl);
 curl_close($curl);
 
 $column_name = [];
+
 $final_data = [];
+
 $data_array = array_map("str_getcsv", explode("\n", $resp));
 
 $labels = array_shift($data_array);
@@ -22,7 +24,9 @@ foreach ($labels as $label) {
     $column_name[] = $label;
 }
 
-for ($j = 0;$j < count($data_array) - 1;$j++) {
+$count = count($data_array) - 1;
+
+for ($j = 0; $j < $count; $j++) {
     $data = array_combine($column_name, $data_array[$j]);
     $final_data[$j] = $data;
 }
@@ -32,8 +36,9 @@ header("Content-type: application/json");
 http_response_code(200);
 
 echo json_encode(array(
-    "ok" => true,
+    "ok"=> true, 
     "status" => 200,
-    "result" => "Hello World" ,
+    "result" => $final_data, 
 ));
+
 ?>
