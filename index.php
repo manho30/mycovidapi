8eef8c3b80dc7046fb89c60546de592633f9e89d
manho <86037446+manho30@users.noreply.github.com>
2022-03-05 01:51:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<!DOCTYPE html>
+<head>
+  <meta name="description" content="MyCovid API is an HTTP request based interface created by PHP for developers easier on building programme. Powered by MoH Malaysia">
+  <meta name="keywords" content="api, COVID-19 API free, malaysia covid data, covid api, covid-19 json api, covid19, COVID vaccine"> 
+  <title> MyCovid API </title>
+</head>
 <?php 
 http_response_code(303);
 
