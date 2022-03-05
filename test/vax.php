fd87a04ee68b1b56b77203cb101b57a45781fd1a
manho <86037446+manho30@users.noreply.github.com>
2022-03-05 19:55:25 +0800

    Update vax.php

--- test/vax.php
+++ test/vax.php
@@ -111,7 +111,7 @@ if(!$query || $query == "now"){
             header("Content-type: application/json");
             http_response_code(200);
 
-            if (!$beautify) {
+            if (!$beautify || $beautify == "false") {
 
                 echo json_encode(array(
                     "ok"=> true, 
