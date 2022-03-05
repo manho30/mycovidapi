686b4505c1fc2930f0b25eec828cae37a1cb7c0c
manho <86037446+manho30@users.noreply.github.com>
2022-03-05 10:33:39 +0800

    Update method.php

--- test/method.php
+++ test/method.php
@@ -4,12 +4,12 @@ if ($_SERVER['REQUEST_METHOD'] != 'GET' || $_SERVER['REQUEST_METHOD'] != 'POST')
         "ok"=> false, 
         "status"=>405, 
         "message"=> "HTTP request not allow! Please use POST and GET method instead!", 
-    )) 
+    ));
 } else {
     echo json_encode(array(
         "ok"=> true, 
         "status"=> 200, 
         "message"=> "HTTP success", 
-    ))  
+    ));
 } 
 ?>
