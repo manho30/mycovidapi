0131fabe8c5289ba355278269fb66536aff5d9bd
manho <86037446+manho30@users.noreply.github.com>
2022-03-05 10:28:30 +0800

    Create method.php

---
+++ test/method.php
@@ -0,0 +1,15 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] != 'GET' || $_SERVER['REQUEST_METHOD'] != 'POST') {
+    echo json_encode(array(
+        "ok": false, 
+        "status": 405, 
+        "message": "HTTP request not allow! Please use POST and GET method instead!", 
+    )) 
+} else {
+    echo json_encode(array(
+        "ok": true, 
+        "status": 200, 
+        "message": "HTTP success", 
+    ))  
+} 
+?>
