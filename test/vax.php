487212e7225711287b4725361a7e0f79b9d59088
manho <86037446+manho30@users.noreply.github.com>
2022-03-06 09:05:56 +0800

    Update vax.php

--- test/vax.php
+++ test/vax.php
@@ -2,73 +2,127 @@
 // query from http request
 $query = $_GET["date"];
 $beautify = $_GET["beautify"];
+$state = $_GET["state"];
 
 // return default data (latest data ) if no specify query
 if(!$query || $query == "now"){
-    $url = "https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/vaccination/vax_malaysia.csv";
+    if (!$state){
+        $url = "https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/vaccination/vax_malaysia.csv";
 
-    $curl = curl_init($url);
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-    // for debug only!
-    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        // for debug only!
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
-    $response = curl_exec($curl);
-    curl_close($curl);
+        $response = curl_exec($curl);
+        curl_close($curl);
 
-    // make the csv data to array of arrays form. 
-    $data_array = array_map("str_getcsv", explode("\n", $response));
+        // change the csv data as array of arrays form. 
+        $data_array = array_map("str_getcsv", explode("\n", $response));
 
-    // remove variable elements from the first line
-    $variable_list = array_shift($data_array);
+        // remove variable elements from the first line
+        $variable_list = array_shift($data_array);
 
-    // list all variables in an array.
-    $variable_name = []; 
-    foreach ($variable_list as $variable) {
-        $variable_name[] = $variable;
-    }
+        // list all variables in an array.
+        $variable_name = []; 
+        foreach ($variable_list as $variable) {
+            $variable_name[] = $variable;
+        }
 
-    // add variable name to every single data.
-    $out_data = [];
-    for ($i = 0; $i < count($data_array) - 1; $i++) {
-        $data = array_combine($variable_name, $data_array[$i]);
-        $out_data[$i] = $data;
-    }
+        // add variable name to every single data.
+        $out_data = [];
+        for ($i = 0; $i < count($data_array) - 1; $i++) {
+            $data = array_combine($variable_name, $data_array[$i]);
+            $out_data[$i] = $data;
+        }
 
-    // return the last element from array 
-    $find_latest_data = $out_data[count(json_decode(json_encode($out_data))) - 1];
-    $final_data = $find_latest_data;
+        // return the last element from array 
+        $find_latest_data = $out_data[count(json_decode(json_encode($out_data))) - 1];
+        $final_data = $find_latest_data;
 
-    if (!$beautify){
+        // no beautify requested 
+        if (!$beautify){
 
-        // header for return json data.
-        header("Content-type: application/json");
-        http_response_code(200); 
+            // header for return json data.
+            header("Content-type: application/json");
+            http_response_code(200); 
 
-        echo json_encode(array(
-            "ok"=> true, 
-            "status" => 200,
-            "result" => $final_data
-        ));
-        die();
+            echo json_encode(array(
+                "ok"=> true, 
+                "status" => 200,
+                "result" => $final_data
+            ));
+            die();
 
+        } else {
+
+            // header for return json data.
+            header("Content-type: application/json");
+            http_response_code(200);
+
+            echo json_encode(array(
+                "ok"=> true, 
+                "status" => 200,
+                "result" => $final_data
+            ), JSON_PRETTY_PRINT);
+
+            die();
+        }
+
+    // for request state data
     } else {
+        $url = "https://raw.githubusercontent.com/MoH-Malaysia/covid19-public/main/vaccination/vax_state.csv";
+
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        // for debug only!
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+
+        // change the csv data as array of arrays form. 
+        $data_array = array_map("str_getcsv", explode("\n", $response));
+
+        // remove variable elements from the first line
+        $variable_list = array_shift($data_array);
+
+        // list all variables in an array.
+        $variable_name = []; 
+        foreach ($variable_list as $variable) {
+            $variable_name[] = $variable;
+        }
+
+        // add variable name to every single data.
+        $out_data = [];
+        for ($i = 0; $i < count($data_array) - 1; $i++) {
+            $data = array_combine($variable_name, $data_array[$i]);
+            $out_data[$i] = $data;
+        }
+
+        // return the last element from array 
+        $state_array = array_chunk($out_data, 16);
+        $find_latest_data = $out_data[count(json_decode(json_encode($state_data))) - 1];
+        $final_data = $find_latest_data;
 
         // header for return json data.
         header("Content-type: application/json");
         http_response_code(200);
 
         echo json_encode(array(
-            "ok"=> true, 
-            "status" => 200,
-            "result" => $final_data
-        ), JSON_PRETTY_PRINT);
+                "ok"=> true, 
+                "status" => 200,
+                "result" => $final_data
+            ), JSON_PRETTY_PRINT);
 
-        die();
-
-    }
+        die(); 
+    } 
 } else {
     if($query){
         if($query == 'all'){
