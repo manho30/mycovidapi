1fcf27c92c7ed7401432f038d7992a594a77b5a2
manho <86037446+manho30@users.noreply.github.com>
2022-03-20 09:10:07 +0800

    Create source.php

---
+++ test/src/path/source.php
@@ -0,0 +1,35 @@
+<?php
+
+$repo = "https://raw.githubusercontent.com/CITF-Malaysia/citf-public/main/";
+
+/** vaccination path **/
+$vax_my = $repo . "vaccination/vax_malaysia.csv";
+$vax_state = $repo . "vaccination/vax_state.csv";
+$vax_school = $repo . "vaccination/vax_school.csv";
+
+/** epidemic path **/
+$case_my = $repo . "epidemic/cases_malaysia.csv";
+$case_state = $repo . "epidemic/cases_state.csv";
+$case_age = $repo . "epidemic/cases_age.csv";
+
+$death_my = $repo . "epidemic/deaths_malaysia.csv";
+$death_state = $repo . "epidemic/deaths_state.csv";
+$death_age  = $repo . "epidemic/deaths_age.csv";
+
+$hospital = $repo . "epidemic/hospital.csv";
+$pkrc = $repo . "epidemic/pkrc.csv";
+$icu = $repo . "epidemic/icul.csv";
+$cluster = $repo . "epidemic/clusters.csv";
+
+$test_my = $repo . "epidemic/tests_malaysia.csv";
+$test_state = $repo . "epidemic/tests_state.csv";
+
+/** population **/
+$pop = $repo . "static/population.csv";
+
+/** mysejahtera **/
+$mysejahtera_checkin = $repo . "mysejahtera/checkin_malaysia.csv";
+$mysejahtera_checkin_time = $repo . "mysejahtera/checkin_malaysia_time.csv";
+$mysejahtera_checkin_state = $repo . "mysejahtera/checkin_state.csv";
+$mysejahtera_trace = $repo . "mysejahtera/trace_malaysia.csv";
+?>
